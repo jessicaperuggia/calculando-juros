e4f74a95e075b48fa877ad03fc2c9a18ce3f70c4
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-08-19 16:57:14 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Olá function componets!</h1>
+    </div>
+  )
+
+}
---
+++ src/components/Installments.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Installments() {
+    return (
+        <div>
+
+        </div>
+    )
+}
